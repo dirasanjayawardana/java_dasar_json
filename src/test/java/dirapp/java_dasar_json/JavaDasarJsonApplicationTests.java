643958694cfebb90de379c7989e9680885089ccd
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-08-23 21:25:45 +0700

    backup

---
+++ src/test/java/dirapp/java_dasar_json/JavaDasarJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDasarJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
