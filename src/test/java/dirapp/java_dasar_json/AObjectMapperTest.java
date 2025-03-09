52753694f69244eb0f85bef16d894eae22a4355e
dirasanjayawardana <dirawardana.dw@gmail.com>
2025-03-09 13:24:16 +0700

    update

--- src/test/java/dirapp/java_dasar_json/AObjectMapperTest.java
+++ src/test/java/dirapp/java_dasar_json/AObjectMapperTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ObjectMapperTest {
+public class AObjectMapperTest {
   // ObjectMapper adalah class di library Jackson sebagai class utama
   // ObjectMapper hanya perlu dibuat sekali dalam sebuah project, ObjectMapper sudah thread save
 
